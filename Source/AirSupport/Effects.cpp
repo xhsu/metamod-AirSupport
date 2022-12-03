2da08362c3c4ea85b7c9b2b93ae084b0c9813258
xhsu <xujiananxm@gmail.com>
2022-12-03 14:52:24 +1100

    HOTFIX compatibility issue with fov-related plugins

--- Source/AirSupport/Effects.cpp
+++ Source/AirSupport/Effects.cpp
@@ -8,6 +8,7 @@ import util;
 
 import Effects;
 import Math;
+import Projectile;
 import Query;
 import Resources;
 
@@ -1008,11 +1009,10 @@ Task CFuelAirCloud::Task_PlayerSuffocation(CBasePlayer *pPlayer, entvars_t *pevW
 		);
 
 		int const iFOV = (i % 2) ? UTIL_Random(74, 79) : UTIL_Random(80, 85);
-		int const iFOVDelta = iFOV - pPlayer->m_iFOV;
 
 		for (; pPlayer->m_iFOV != iFOV && pPlayer->IsAlive();)
 		{
-			(iFOVDelta > 0) ? ++pPlayer->m_iFOV : --pPlayer->m_iFOV;
+			((iFOV - pPlayer->m_iFOV) > 0) ? ++pPlayer->m_iFOV : --pPlayer->m_iFOV;	// don't make the FOV-delta a constant and only eval once. It could cause compatibility issue.
 
 			co_await TaskScheduler::NextFrame::Rank.back();
 		}
@@ -1058,6 +1058,9 @@ void CFuelAirCloud::OnTraceAttack(TraceResult const &tr, EHANDLE<CBaseEntity> pS
 	);
 
 	if (s_rgpAeroClouds.empty())
+		return;
+
+	if (auto pHitEntity = EHANDLE<CBaseEntity>(tr.pHit); pHitEntity.Is<CFuelAirExplosive>())
 		return;
 
 	for (auto &&pCloud : s_rgpAeroClouds |
--- Source/AirSupport/Weapon.cpp
+++ Source/AirSupport/Weapon.cpp
@@ -184,13 +184,13 @@ void __fastcall HamF_Item_PostFrame(CBasePlayerItem *pItem, int) noexcept
 	}
 	else if (pThis->m_pPlayer->m_afButtonPressed & IN_USE) [[unlikely]]
 	{
-		//g_engfuncs.pfnMakeVectors(pThis->m_pPlayer->pev->v_angle);
+		g_engfuncs.pfnMakeVectors(pThis->m_pPlayer->pev->v_angle);
 
-		//Prefab_t::Create<CBullet>(
-		//	pThis->m_pPlayer->GetGunPosition() + gpGlobals->v_forward * 64.0,
-		//	gpGlobals->v_forward * 2048,
-		//	pThis->m_pPlayer
-		//);
+		Prefab_t::Create<CBullet>(
+			pThis->m_pPlayer->GetGunPosition() + gpGlobals->v_forward * 64.0,
+			gpGlobals->v_forward * 2048,
+			pThis->m_pPlayer
+		);
 
 		//TraceResult tr{};
 		//auto const vecSrc = pThis->m_pPlayer->GetGunPosition();
